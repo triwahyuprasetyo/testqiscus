0169c2543b1e0280c9e432c716aa19bb0abd81c1
why <triwahyuprasetyo@gmail.com>
2020-09-03 13:07:16 +0700

    add TestQiscusApplication & add uses permission internet

---
+++ app/src/main/java/com/wahyu/testqiscus/TestQiscusApplication.kt
@@ -0,0 +1,15 @@
+package com.wahyu.testqiscus
+
+import android.app.Application
+import com.qiscus.sdk.chat.core.QiscusCore
+
+class TestQiscusApplication : Application() {
+
+    private val APPID: String = "sdksample"
+
+    override fun onCreate() {
+        super.onCreate()
+        QiscusCore.setup(this, APPID)
+    }
+
+}
