e10370aa88912b22e4cba85ce5178582e9bf6dcc
why <triwahyuprasetyo@gmail.com>
2020-09-04 21:43:30 +0700

    test listen message

--- app/src/main/java/com/wahyu/testqiscus/ui/fragment/ChatDetailFragment.kt
+++ app/src/main/java/com/wahyu/testqiscus/ui/fragment/ChatDetailFragment.kt
@@ -10,9 +10,12 @@ import androidx.fragment.app.FragmentActivity
 import com.qiscus.sdk.chat.core.data.model.QiscusChatRoom
 import com.qiscus.sdk.chat.core.data.model.QiscusComment
 import com.qiscus.sdk.chat.core.data.remote.QiscusApi
+import com.qiscus.sdk.chat.core.data.remote.QiscusPusherApi
+import com.qiscus.sdk.chat.core.event.QiscusChatRoomEvent
 import com.qiscus.sdk.chat.core.event.QiscusCommentReceivedEvent
 import com.wahyu.testqiscus.ConstantVariable
 import com.wahyu.testqiscus.R
+import com.wahyu.testqiscus.Utils
 import kotlinx.android.synthetic.main.fragment_chat_detail.view.*
 import org.greenrobot.eventbus.EventBus
 import org.greenrobot.eventbus.Subscribe
@@ -40,8 +43,48 @@ class ChatDetailFragment : Fragment() {
 
     @Subscribe
     fun onMessageReceived(event: QiscusCommentReceivedEvent) {
-        println("message : " + event.qiscusComment.message) // to get the comment
+        println("message : " + event.qiscusComment.message + " - " + event.qiscusComment.sender) // to get the comment
+        val context: Context = activity as Context
+        Utils.showToast(
+            context,
+            event.qiscusComment.message + " - " + event.qiscusComment.senderEmail + " - " + event.qiscusComment.state
+        )
+//        QiscusCore.getDataStore().addOrUpdate(qiscusChatRoom)
+        /*object : CountDownTimer(ConstantVariable.SPLASH_TIME, 1000) {
+            override fun onTick(m: Long) {}
+            override fun onFinish() {
+                QiscusPusherApi.getInstance()
+                    .markAsRead(event.qiscusComment.roomId, event.qiscusComment.id)
+            }
+        }.start()*/
+
     }
+
+    @Subscribe
+    fun onReceiveRoomEvent(roomEvent: QiscusChatRoomEvent) {
+        when (roomEvent.event) {
+            QiscusChatRoomEvent.Event.TYPING -> {
+                roomEvent.roomId // this is the room id
+                roomEvent.user // this is the qiscus user id
+                roomEvent.isTyping // true if the user is typing
+
+            }
+            QiscusChatRoomEvent.Event.DELIVERED -> {
+                roomEvent.roomId // this is the room id
+                roomEvent.user // this is the qiscus user id
+                roomEvent.commentId // the comment id was delivered
+                println("DELIVEREDDDDDDD " + roomEvent.user)
+            }
+            QiscusChatRoomEvent.Event.READ -> {
+                roomEvent.roomId // this is the room id
+                roomEvent.user // this is the qiscus user id
+                roomEvent.commentId // the comment id was read
+                println("READDDDDDDDDDDD " + roomEvent.user)
+            }
+        }
+    }
+
+    var qiscusChatRoom: QiscusChatRoom? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -56,9 +99,23 @@ class ChatDetailFragment : Fragment() {
             }
         })
 
-        var qiscusChatRoom: QiscusChatRoom? = arguments?.get("qiscusChatRoom") as QiscusChatRoom?
+        qiscusChatRoom = arguments?.get("qiscusChatRoom") as QiscusChatRoom?
         qiscusChatRoom?.let {
             view.toolbar.title = it.name
+        }
+        QiscusPusherApi.getInstance().subscribeChatRoom(qiscusChatRoom)
+
+        val context: Context = activity as Context
+
+        view.buttonSend.setOnClickListener {
+            val text: String = view.editTextTextInput.text.toString().trim()
+            if (text.isNotEmpty()) {
+                qiscusChatRoom?.let {
+                    sendMessage(text, qiscusChatRoom!!.id)
+                }
+            } else {
+                Utils.showToast(context, getString(R.string.message_alert))
+            }
         }
 
         return view
